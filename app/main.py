fb8b0173e6fb0f1e1cc8b742e6059f0be2f9fd8f
Грицик Евгений <ev.gritsik@localhost.local>
2026-05-14 20:01:40 +0800

    use proxy

--- app/main.py
+++ app/main.py
@@ -1,14 +1,26 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
 
-from app.config import BOT_TOKEN
+from aiohttp_socks import ProxyConnector
+
+from app.config import BOT_TOKEN, PROXY_URL
 from app.handlers.status import router
 
 
 async def main():
 
-    bot = Bot(BOT_TOKEN)
+    if PROXY_URL:
+        connector = ProxyConnector.from_url(PROXY_URL)
+        session = AiohttpSession(connector=connector)
+    else:
+        session = AiohttpSession()
+
+    bot = Bot(
+        token=BOT_TOKEN,
+        session=session
+    )
 
     dp = Dispatcher()
 
